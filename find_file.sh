21e110b4aad2bb21f0f51d3620485f27c16dbac6
Ricardo Taboada Naranjo <ricardo@elemento43.com>
2020-01-28 13:59:31 -0500

    feat: Script v 1.0.0

---
+++ find_file.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# -e exit       when a command fails
+# -u exit       when trying to use undefined variable
+# -o pipefail   return the exit code of piped commands that fail
+# -x            debug
+
+set -euo pipefail
+
+PREFIX="[*]"
+
+echo
+echo $PREFIX "Good commit: $1"
+echo $PREFIX "File path: $2"
+echo $PREFIX "Git branch: $3"
+echo $PREFIX "Project name $4"
+# conditional variables
+BG=''
+FOUND=2
+
+# init git bisect
+echo 
+echo $PREFIX "Init git bisect"
+
+cd $4
+git checkout $3
+git bisect start
+git bisect bad
+git bisect good $1
+cd ..
+
+#-eq $FOUND is numerically equal to 2 ?
+while [ $FOUND -eq 2 ]; do 
+    # check the file if it exists
+    # -f check if the file is a regular file 
+    if [ -f $2 ] ; then
+        BG='bad'
+        echo $PREFIX "File found" 
+    else
+        BG='good'
+        echo $PREFIX "File not found"
+    fi
+    cd $4
+    # git bisect bad/good   Parameters, return a view  
+    # wc                    Print newline, word, and byte counts for each FILE.              
+    # awk                   $1 newlines number,$2 words number, $3 byte number.
+    # assign the number of lines of the output of git biset bad/good to the ENCONTRADO variable
+    ENCONTRADO=$(git bisect $BG | wc | awk '$1 {print $1}') 
+    cd ..
+done    
+echo "[*] commit encontrado "
+cd $4
+git show
+
+ 
